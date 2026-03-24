0a9af6956bc3ee6303104174a07f6e8db42882d1
Nischal Tamang <nischaltamang@dhcp-10-250-159-134.harvard.edu>
2026-03-23 21:16:11 -0400

    Trigger redploy

--- lib/api.ts
+++ lib/api.ts
@@ -3,7 +3,6 @@ import { transformCandidate } from "./transform";
 export async function fetchCandidates(){
     // Fetch the candidates from the randomuser.me API
     const res = await fetch("https://randomuser.me/api/?seed=recruitu-takehome&results=50");
-
     if (!res.ok) {
         throw new Error("Failed to fetch candidates");
     }
